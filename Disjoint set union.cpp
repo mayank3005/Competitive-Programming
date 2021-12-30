4191d4f18ba1c13f63ec52dbf41aad71a98c6ed0
Mayank <82897488+mayank3005@users.noreply.github.com>
2021-12-30 11:12:12 +0530

    Create Disjoint set union.cpp

---
+++ Disjoint set union.cpp
@@ -0,0 +1,23 @@
+const int N = 1e5 + 7;
+
+int parent[N], size[N];
+
+void make(int v) {
+	parent[v] = v;
+	size[v] = 1;
+}
+
+int find(int v) {
+	if (v == parent[v]) return v;
+	return parent[v] = find(parent[v]);
+}
+
+void Union(int a, int b) {
+	a = find(a); b = find(b);
+	if (a != b) {
+		if (size[a] < size[b]) swap(a, b);
+		parent[b] = a;
+		size[a] += b;
+	}
+}
+  
