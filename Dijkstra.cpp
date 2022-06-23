88edfcf3dc07834f1d3b94a4446ec25289ab5d3c
Mayank <82897488+mayank3005@users.noreply.github.com>
2022-06-23 12:58:32 +0530

    Create Dijkstra.cpp

---
+++ Dijkstra.cpp
@@ -0,0 +1,24 @@
+/*
+  Single source shortest path algorithm
+  Time complexity : 0(Vlogv + ElogV)
+  Space complexity : 0(V+V)
+
+*/
+void dijkstra(vector<pair<int, int>> adj[], int n, int st) {
+	vector<int> dist(n, 1e8);
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	dist[st] = 0;
+	pq.push({0, st});
+	while (!pq.empty()) {
+		int src = pq.top().second, d = pq.top().first;
+		pq.pop();
+		if (dist[src] != d) continue;
+		for (auto &[dest, wt] : adj[src]) {
+			if (dist[src] + wt < dist[dest]) {
+				dist[dest] = dist[src] + wt;
+				pq.push({dist[dest], dest});
+			}
+		}
+	}
+	for (auto i : dist) cout << i << " ";
+}
