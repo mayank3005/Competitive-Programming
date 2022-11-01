112cd25ee69e5957c48723e474e5381a2013a1f6
Mayank <82897488+mayank3005@users.noreply.github.com>
2022-11-01 08:59:48 +0530

    Create rabinKarp.cpp

--- rabinKarp.cpp
+++ rabinKarp.cpp
@@ -1,33 +1,43 @@
+#define nline '\n'
 const int MOD = 1e3 + 7;
 const int d = 256;
 
-void RabinKarp(string &txt, string &pat) {
-	int m = pat.length(), n = txt.length();
+void RabinKarp(string &txt, string &pat)
+{
+    int m = pat.length(), n = txt.length();
 
-	int hashp = 0, hasht = 0; // hash values for pattern and text
-	int h = 1; // used for rolling hash function
-	int i, j;
+    int hashp = 0, hasht = 0; // hash values for pattern and text
+    int h = 1;                // used for rolling hash function
+    int i, j;
 
-	for (i = 0; i < m - 1; i++) {
-		h = (h * d) % MOD;
-	}
+    for (i = 0; i < m - 1; i++)
+    {
+        h = (h * d) % MOD;
+    }
 
-	for (i = 0; i < m; i++) {
-		hashp = (hashp * d % MOD + pat[i]) % MOD;
-		hasht = (hasht * d % MOD + txt[i]) % MOD;
-	}
+    for (i = 0; i < m; i++)
+    {
+        hashp = (hashp * d % MOD + pat[i]) % MOD;
+        hasht = (hasht * d % MOD + txt[i]) % MOD;
+    }
 
-	for (i = 0; i < n - m; i++) {
-		if (hashp == hasht) {
-			for (j = 0; j < m; j++) {
-				if (txt[i + j] != pat[j]) break;
-			}
-			if (j == m) cout << "pattern found at : " << i << nline;
-		}
-		if (i < n - m) {
-			hasht = (d * (hasht - txt[i] * h) % MOD + txt[i + m]) % MOD;
-			if (hasht < 0)
-				hasht = hasht + MOD;
-		}
-	}
+    for (i = 0; i <= n - m; i++)
+    {
+        if (hashp == hasht)
+        {
+            for (j = 0; j < m; j++)
+            {
+                if (txt[i + j] != pat[j])
+                    break;
+            }
+            if (j == m)
+                cout << "pattern found at : " << i << nline;
+        }
+        if (i < n - m)
+        {
+            hasht = (d * (hasht - txt[i] * h) % MOD + txt[i + m]) % MOD;
+            if (hasht < 0)
+                hasht = hasht + MOD;
+        }
+    }
 }
