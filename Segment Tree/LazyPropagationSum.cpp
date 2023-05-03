36f98d065f43006c1e241fce66dfbf1a973e0f6f
Mayank <82897488+mayank3005@users.noreply.github.com>
2023-05-03 15:53:47 +0530

    Update LazyPropagationSum.cpp

--- Segment Tree/LazyPropagationSum.cpp
+++ Segment Tree/LazyPropagationSum.cpp
@@ -1,37 +1,46 @@
-const int N = 2e5 + 5;
-int tree[4 * N], lazy[4 * N];
+const int N = 2e5;
+int tree[4 * N]; int lazy[4 * N]; vector<int> a;
 
-void build(int si, int sl, int sr, vector<int> &v) {
-	if (sl == sr) {
-		tree[si] = v[sl]; lazy[si] = 0; return;
+void build(int lx, int rx, int x) {
+	if (lx == rx) {
+		tree[x] = a[lx]; return;
 	}
-	int mid = (sl + sr) >> 1;
-	build(2 * si + 1, sl, mid, v);
-	build(2 * si + 2, mid + 1, sr, v);
-	tree[si] = tree[2 * si + 1] + tree[2 * si + 2]; lazy[si] = 0;
+	int mid = (lx + rx) >> 1;
+	build(lx, mid, 2 * x + 1); build(mid + 1, rx, 2 * x + 2);
+	tree[x] = tree[2 * x + 1] + tree[2 * x + 2];
 }
 
-void update(int si, int sl, int sr, int l, int r, int val) {
-
-	if (lazy[si]) {
-		int dx = lazy[si]; lazy[si] = 0;
-		tree[si] += dx * (sr - sl + 1);
-		if (sl != sr) {
-			lazy[2 * si + 1] += dx, lazy[2 * si + 2] += dx;
-		}
+int query(int lx, int rx, int x, int l, int r) {
+	if (lazy[x]) {
+		int dx = lazy[x]; lazy[x] = 0;
+		tree[x] += dx * (rx - lx + 1);
+		if (lx != rx)
+			lazy[2 * x + 1] += dx, lazy[2 * x + 2] += dx;
 	}
 
-	if (sr<l or sl>r) return ;
-	if (sl >= l and sr <= r) {
-		tree[si] += (sr - sl + 1) * val;
-		if (sl != sr) {
-			lazy[2 * si + 1] += val, lazy[2 * si + 2] += val;
-		}
+	if (r<lx or l>rx) return 0;
+	if (lx >= l and rx <= r) return tree[x];
+	int mid = (lx + rx) >> 1;
+	return query(lx, mid, 2 * x + 1, l, r) + query(mid + 1, rx, 2 * x + 2, l, r);
+}
+
+void update(int lx, int rx, int x, int l, int r, int v) {
+	if (lazy[x]) {
+		int dx = lazy[x]; lazy[x] = 0;
+		tree[x] += dx * (rx - lx + 1);
+		if (lx != rx)
+			lazy[2 * x + 1] += dx, lazy[2 * x + 2] += dx;
+	}
+	if (r<lx or l>rx) return;
+	if (lx >= l and rx <= r) {
+		int dx = (rx - lx + 1) * v;
+		tree[x] += dx;
+		if (lx != rx)
+			lazy[2 * x + 1] += v, lazy[2 * x + 2] += v;
 		return;
 	}
-
-	int mid = (sl + sr) >> 1;
-	update(2 * si + 1, sl, mid, l, r, val);
-	update(2 * si + 2, mid + 1, sr, l, r, val);
-	tree[si] = tree[2 * si + 1] + tree[2 * si + 2];
+	int mid = (lx + rx) >> 1;
+	update(lx, mid, 2 * x + 1, l, r, v);
+	update(mid + 1, rx, 2 * x + 2, l, r, v);
+	tree[x] = tree[2 * x + 1] + tree[2 * x + 2];
 }
